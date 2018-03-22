9b80fec7cf39bb6742a343f76ae42acfe3007943
DanBonehill <DanBonehill@me.com>
2018-03-22 17:03:32 +0000

    Import assets and setup GoalsVC UI

--- goalpost/Controller/GoalsVC.swift
+++ goalpost/Controller/GoalsVC.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class GoalsVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func addGoalBtnPressed(_ sender: Any) {
     }
 }
