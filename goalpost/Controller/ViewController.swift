7e8d2ec64dfca0c3a794bab72a19678f2a1ffc02
DanBonehill <DanBonehill@me.com>
2018-03-22 16:48:11 +0000

    Rename and organise files

---
+++ goalpost/Controller/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GoalsVC.swift
+//  goalpost
+//
+//  Created by Daniel Bonehill on 22/03/2018.
+//  Copyright © 2018 Daniel Bonehill. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
