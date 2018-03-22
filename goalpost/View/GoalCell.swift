68a027de78a24ea734af042099431acfb9ea897b
DanBonehill <DanBonehill@me.com>
2018-03-22 17:42:27 +0000

    Create GoalCell and prototype cell

---
+++ goalpost/View/GoalCell.swift
@@ -0,0 +1,22 @@
+//
+//  GoalCell.swift
+//  goalpost
+//
+//  Created by Daniel Bonehill on 22/03/2018.
+//  Copyright © 2018 Daniel Bonehill. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+    
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+}
